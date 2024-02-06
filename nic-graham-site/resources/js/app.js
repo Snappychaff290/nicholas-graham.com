90ae4a60e8949d6a6f9ad83a7cec0b26bfa9a6e8
Nic Laptop <nagraham4@gmail.com>
2024-02-06 09:03:17 -0500

    some background effects

--- nic-graham-site/resources/js/app.js
+++ nic-graham-site/resources/js/app.js
@@ -1,11 +1,12 @@
-import "./bootstrap";
+//import "./bootstrap";
 
-document.getElementById("cards").onmousemove = (e) => {
-    for (const card of document.getElementsByClassName("card")) {
+document.getElementById("test").onmousemove = (e) => {
+    for (const card of document.getElementsByClassName("testy")) {
         const rect = card.getBoundingClientRect(),
             x = e.clientX - rect.left,
             y = e.clientY - rect.top;
         card.style.setProperty("--mouse-x", `${x}px`);
         card.style.setProperty("--mouse-y", `${y}px`);
+        console.log("HERE");
     }
 };
