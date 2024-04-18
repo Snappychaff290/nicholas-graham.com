7e3415d4ffffd5df3e5d69e7dce4a3a297f208a2
Nic Laptop <nagraham4@gmail.com>
2024-04-18 08:16:41 -0400

    vite blah

--- nic-graham-site/vite.config.js
+++ nic-graham-site/vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/style.css', 'resources/css/style_mobile.css', 'resources/css/placeholder.css', 'resources/css/old_site/overview.css', 'resources/css/old_site/style-mobile.css', 'resources/css/old_site/style-small_screen.css', 'resources/css/old_site/style.css', 'resources/js/app.js', 'resources/js/old_site/index.js'],
+            input: [
+                "resources/css/style.css",
+                "resources/css/style_mobile.css",
+                "resources/css/placeholder.css",
+                "resources/css/old_site/overview.css",
+                "resources/css/old_site/style-mobile.css",
+                "resources/css/old_site/style-small_screen.css",
+                "resources/css/old_site/style.css",
+                "resources/js/app.js",
+                "resources/js/old_site/index.js",
+            ],
             refresh: true,
         }),
     ],
